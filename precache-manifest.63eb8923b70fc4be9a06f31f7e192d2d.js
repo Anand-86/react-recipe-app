492bc5e2cfd2a99780549651b99f30f2e88afe13
Anand Ramdhani <anandkr.github@gmail.com>
2020-03-07 18:11:44 +0200

    Updates

---
+++ precache-manifest.63eb8923b70fc4be9a06f31f7e192d2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5746c78eaa2d2173df2ac5a5c78be7c2",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "9e42904b1881aa9d47e0",
+    "url": "/react-recipe-app/static/css/main.cac86597.chunk.css"
+  },
+  {
+    "revision": "ae73f7e7a393a2fba5d7",
+    "url": "/react-recipe-app/static/js/2.c210351a.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-recipe-app/static/js/2.c210351a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e42904b1881aa9d47e0",
+    "url": "/react-recipe-app/static/js/main.2781233b.chunk.js"
+  },
+  {
+    "revision": "6180a355107d5504581f",
+    "url": "/react-recipe-app/static/js/runtime-main.4e06ec67.js"
+  }
+]);
